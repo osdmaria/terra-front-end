5d0a499cbcd56ced07c5a9d34f510d2f4a75c9e8
Sami Brahim Djelloul <61477348+samuaws@users.noreply.github.com>
2022-05-13 19:12:41 +0100

    haki maria

---
+++ src/views/SignUp.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="signup">
+      <div class="div-left">
+          <h1>Sign up</h1>
+          
+      </div>
+      <div class="div-right">
+
+      </div>
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+     name: "SignUp",
+  components: {
+    NavBar,
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
